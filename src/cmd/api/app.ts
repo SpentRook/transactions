1b4ac2a970851934af4cf668483ea5cd621227e5
Richard Lion Guevara Puerta <leonvilla6@outlook.com>
2024-03-08 01:33:16 -0500

    Add CORS and Swagger documentation

--- src/cmd/api/app.ts
+++ src/cmd/api/app.ts
@@ -1,10 +1,20 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import cors from "./config/cors";
+import swaggerSpec from "./config/swagger";
+import { routes } from "./route";
 
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors)
 
+app.use("/api/v1", routes)
+app.use("/api/docs/", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use((req, res) => {
+    res.status(404).json({ message: "Not Found path: " + req.url});
+});
 
 export default app
